9a83c9427ce2970bdac5e614daef62fa49e377e1
Sonu Gupta <rg3029926@gmail.com>
2023-03-20 03:07:39 +0530

    electron js

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 const { app, BrowserWindow, Menu } = require("electron");
-
+const path= require('path')
 function cWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 700,
     webPreferences: {
-      nodeIntegration: true,
+    //   nodeIntegration: true,
+      preload:path.join(__dirname, 'preload.js')
     },
   });
   win.loadFile("index.html");
-  win.webContents.openDevTools();
+//   win.webContents.openDevTools();
+
 }
 
-app.whenReady().then(cWindow);
+app.whenReady().then(() => {
+    cWindow();
+
+    app.on('activate', () => {
+        if(BrowserWindow.getAllWindows().length === 0 ) cWindow()
+    })
+
+
+});
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') app.quit()
+})
