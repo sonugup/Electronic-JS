e2cb1d3ff4d8d902a6a86af27e348b44209e684d
Sonu Gupta <rg3029926@gmail.com>
2023-03-19 19:54:22 +0530

    Electron js

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-const {app, BrowserWindow} = require("electron");
+const {app, BrowserWindow, Menu} = require("electron");
 
 function cWindow() {
     const win= new BrowserWindow({
-        width:500,
-        height:500,
+        width:800,
+        height:700,
+        // frame:false,
         webPreferences:{
             nodeIntegration:true
         }
     })
     win.loadFile("index.html")
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
 }
 
 app.whenReady().then(cWindow)
