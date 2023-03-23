9e0400c1eea88ee35149a151557782ac82d4d013
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-03-23 22:45:23 +0530

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -3,6 +3,7 @@ document.getElementById('toggle-dark-mode').addEventListener('click', async () =
     document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
   })
   
+
   document.getElementById('reset-to-system').addEventListener('click', async () => {
     await window.darkMode.system()
     document.getElementById('theme-source').innerHTML = 'System'
