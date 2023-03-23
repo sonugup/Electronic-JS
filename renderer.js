de64d4ae50e02c8eba76cfbf7763f0070808c19d
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-03-23 22:45:50 +0530

    Create renderer.js

--- renderer.js
+++ renderer.js
@@ -3,8 +3,8 @@ document.getElementById('toggle-dark-mode').addEventListener('click', async () =
     document.getElementById('theme-source').innerHTML = isDarkMode ? 'Dark' : 'Light'
   })
   
-
   document.getElementById('reset-to-system').addEventListener('click', async () => {
     await window.darkMode.system()
     document.getElementById('theme-source').innerHTML = 'System'
   })
+
