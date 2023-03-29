25b0f9097e91606c88301398c385cdfffb1457d6
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-03-30 01:28:31 +0530

    Update index.js

--- Timer-app/index.js
+++ Timer-app/index.js
@@ -11,7 +11,7 @@ function createWindow () {
 
     win.loadFile("index.html")
 
-    win.webContents.openDevTools()
+//     win.webContents.openDevTools()
     win.on('closed', () => {
         win=null;
     })
