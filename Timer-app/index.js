04c85809de656185223236ecdf41229ec996c442
Sonu Gupta <rg3029926@gmail.com>
2023-03-29 23:43:44 +0530

    electron

---
+++ Timer-app/index.js
@@ -0,0 +1,29 @@
+const {app,BrowserWindow }=require("electron")
+
+const path=require("electron")
+
+function createWindow () {
+    const win=new BrowserWindow({
+        width:500,
+        height:400,
+        resizable:false
+    })
+
+    win.loadFile("index.html")
+
+    win.webContents.openDevTools()
+    win.on('closed', () => {
+        win=null;
+    })
+}
+
+
+app.on('ready', createWindow)
+
+
+
+app.on("window-all-closed", ()=> {
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+})
