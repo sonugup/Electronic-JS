792edc15a386d1c88a2c5c70ec77bc2322cc74fc
Sonu Gupta <rg3029926@gmail.com>
2023-03-30 13:00:22 +0530

    Timer Desktop

--- Timer-app/index.js
+++ Timer-app/index.js
@@ -1,26 +1,37 @@
-const {app,BrowserWindow }=require("electron")
-const path=require("electron")
 
-function createWindow () {
-    const win=new BrowserWindow({
-        width:500,
-        height:400,
-        resizable:false
-    })
-
-    win.loadFile("index.html")
-
-//     win.webContents.openDevTools()
-    win.on('closed', () => {
-        win=null;
-    })
-}
-
-app.on('ready', createWindow)
+const electron = require("electron");
+const path = require("path")
+const BrowserWindow = electron.remote.BrowserWindow
 
 
-app.on("window-all-closed", ()=> {
-    if(process.platform !== 'darwin') {
-        app.quit();
+function timer() {
+    let timer=new Date();
+    let hours=timer.getHours();
+    let mint=timer.getMinutes();
+    let second=timer.getSeconds();
+
+    if(hours>12){
+        hours=hours-12;
     }
-})
+    if(hours===0){
+        hours=12;
+    }
+    if(hours<12){
+        hours="0"+hours;
+    }
+    if(mint<10){
+        mint='0'+mint
+    }
+    if(second<10){
+        second='0'+second;
+    }
+
+     document.getElementById("hour").innerHTML=hours;
+    document.getElementById("minte").innerHTML=mint;
+     document.getElementById("sec").innerHTML=second;
+   
+}
+timer();
+
+setInterval(timer, 1000)
+
