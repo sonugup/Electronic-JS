47b320b88f88f140c14acabef3f75ed8460d979e
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-04-03 19:52:44 +0530

    Update main.js

--- ScreenShot/main.js
+++ ScreenShot/main.js
@@ -16,6 +16,7 @@ function createWindow() {
         }
     })
     mainWindow.loadFile(path.join(__dirname, "index.html"));
+    
     win.webContents.openDevTools()
     mainWindow.on("close", () => {mainWindow=null;});
 }
