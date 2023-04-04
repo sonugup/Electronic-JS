6d78443f274e14c4d30b45fb5f6b405245b72d57
Sonu Gupta <101567095+sonugup@users.noreply.github.com>
2023-04-05 01:09:19 +0530

    Automatically take screenshot

--- ScreenShot/index.js
+++ ScreenShot/index.js
@@ -8,7 +8,6 @@ btn.addEventListener("click", () => {
         const a=document.createElement('a');
         a.setAttribute('download', `imageName.png${Math.floor(Math.random()*10)}`);
         a.setAttribute('href', url);
-
               setInterval(() => {
                 a.click();
             }, 10000);
